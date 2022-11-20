09597ce18d024adddbce225f5982c33ea65a822a
mariem460 <mariem.oueslati.hajjem@gmail.com>
2022-11-20 19:06:43 +0100

    add functionality to progress bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-
 import VoyageProgress from './components/voyageProgress';
 
 function App() {
   const portOfLoading = "Madrid";
   const portOfDischarge = "Denmark";
-  const departureTime = "2022-11-16T11:50:12Z";
-  const arrivalTime = "2022-11-24T13:50:12Z";
+  const departureTime = "2022-11-16T19:10:12Z";
+  const arrivalTime = "2022-11-16T22:50:12Z";
   return (
     <div className="App">
       <p>Dashboard</p>  
--- src/components/voyageProgress.js
+++ src/components/voyageProgress.js
@@ -4,42 +4,55 @@ import '../styles/voyageProgress.css'
 
 function voyageProgress ({portOfLoading, portOfDischarge, departureTime, arrivalTime}) {
     const dotsCount = 14;
-    const arrayOfDots = [...Array(dotsCount).keys()];
+    const dotNumbers = [...Array(dotsCount).keys()];
     const currentTime = new Date().getTime();
-    var totalTime = new Date(arrivalTime).getTime() - new Date(departureTime).getTime();
-    var progress = currentTime - new Date(departureTime).getTime();
-    const timePercentage = Math.round(progress / totalTime * 100 );
+    const totalTime = new Date(arrivalTime).getTime() - new Date(departureTime).getTime();
+    const progress = currentTime - new Date(departureTime).getTime();
 
-    //[true, true, true, faelse, false felae]
+    let progressPercentage = Math.round(progress / totalTime * 100);
+    let indexOfLastInProgress = dotsCount - 1;
 
-    const allDotsDetails = arrayOfDots
-        .map(i => {
+    if (currentTime < new Date(departureTime).getTime()) {
+        progressPercentage = 0;
+    } else if (currentTime > new Date(arrivalTime).getTime()) {
+        progressPercentage = 100;
+    }
+
+    const allDotsDetails = dotNumbers
+        .map((dotNr, index) => {
             let color = "rgb(136, 162, 189)";
-            const dotPercentage = Math.round(i / dotsCount * 100 );
-            let isLastInProgress = false;
-            if (dotPercentage <= timePercentage) {
-                isLastInProgress = true;
+            const dotPercentage = Math.round(dotNr / dotsCount * 100 );
+            if (dotPercentage <= progressPercentage) {
                 color = "rgb(56, 80, 106)";
+                indexOfLastInProgress = index;
             }
             let dotDetails = { isExtremity: false, color};
     
-            if (i === 0 || i === dotsCount - 1) {
+            if (dotNr === 0 || dotNr === dotsCount - 1) {
                 dotDetails = {
                     ...dotDetails,
                     isExtremity: true,
-                    portName: i === 0 ? portOfLoading  : portOfDischarge,
-                    isLastInProgress
+                    portName: dotNr === 0 ? portOfLoading  : portOfDischarge
                 }
             }
             return dotDetails;
-        })
+        });
 
-    let dots = allDotsDetails.map((dotDetails, index) => 
-            <Dot isExtremity = {dotDetails.isExtremity} portName={dotDetails.portName} key={index} color={dotDetails.color} isLastInProgress={dotDetails.isLastInProgress}></Dot>)
+        
+
+    let dots = allDotsDetails.map((dotDetails, index) => {
+        return <Dot
+            isExtremity = {dotDetails.isExtremity}
+            portName={dotDetails.portName}
+            key={index}
+            color={dotDetails.color}
+            isLastInProgress={index === indexOfLastInProgress}>
+        </Dot>
+    })
 
     let pinStyle = {
         position: "relative",
-        left: (timePercentage - 170),
+        left: (progressPercentage - 170),
         height: '50px',
         width: '50px'
     }
