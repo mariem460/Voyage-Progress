133988490f35f1de887ae345da90f4e3aac8845c
mariem460 <mariem.oueslati.hajjem@gmail.com>
2022-11-21 17:18:42 +0100

    add comment

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import VoyageProgress from './components/voyageProgress';
 
 function App() {
+  //here you can modify the date and the time to check the progress
   const portOfLoading = "Madrid";
   const portOfDischarge = "Denmark";
-  const departureTime = "2022-11-20T18:00:12Z";
-  const arrivalTime = "2022-11-20T22:00:12Z";
+  const departureTime = "2022-11-21T16:00:12Z";
+  const arrivalTime = "2022-11-21T18:00:12Z";
   return (
     <div className="App">
       <p>Dashboard</p>  
